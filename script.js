f3c4e53140e6f3750232618dd38c93ee0ab1174c
will2375 <wil.liam_benicio@hotmail.com>
2022-01-14 16:55:26 -0300

    capturar elementos html manipulaveis

---
+++ script.js
@@ -0,0 +1,5 @@
+const image = document.querySelector('img');
+const botao = document.querySelector('button');
+const nomeDoPersonagem = document.querySelector('#nome');
+const nomeDoPersonagem = document.querySelector('#especie');
+const nomeDoPersonagem = document.querySelector('#status');
