f3b7373e585c5ef04bc669c245289dc81258d766
will2375 <wil.liam_benicio@hotmail.com>
2022-01-14 17:44:28 -0300

    corrigir html

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 const image = document.querySelector('img');
-const botao = document.querySelector('button');
+const botao = document.querySelector('#botao');
 const nomeDoPersonagem = document.querySelector('#nome');
-const nomeDoPersonagem = document.querySelector('#especie');
-const nomeDoPersonagem = document.querySelector('#status');
+const especie = document.querySelector('#especie');
+const condicao = document.querySelector('#status');
+
+
+pegarPersonagem = () => {
+    return fetch(`https://rickandmortyapi.com/api/character/2`, {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            "Content-type": 'application/json'
+        }
+    }).then((response) => response.json()).then((data) => {
+        imagem.src = data.image;
+        imagem.alt = data.name;
+        nomeDoPersonagem.innerHTML = data.species;
+        condicao.innerHTML = data.status;
+
+    });
+}
+
+botao.onclick = pegarPersonagem;
